6121046e16392f7f0950e822c47a58579d6fd816
millslf <millslf@gmail.com>
2025-01-19 07:37:21 +1100

    Light refactoring

--- src/main/java/com/frankies/bootcamp/sport/MTB.java
+++ src/main/java/com/frankies/bootcamp/sport/MTB.java
@@ -1,6 +1,6 @@
 package com.frankies.bootcamp.sport;
 
-import com.frankies.bootcamp.model.StravaActivityResponse;
+import com.frankies.bootcamp.model.strava.StravaActivityResponse;
 
 public class MTB extends DistanceSport {
     public final static Double CALCULATED_DISTANCE_MULTIPLIER = 0.5;
